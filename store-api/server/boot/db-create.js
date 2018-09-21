a93980ae74509fceeed3c7902d26396f0b9c0e92
Cristian Bullokles <cristian.bullokles@gmail.com>
2018-09-20 21:40:16 -0300

    Fix npm install on Heroku

--- store-api/server/boot/db-create.js
+++ store-api/server/boot/db-create.js
@@ -1,8 +1,4 @@
 'use strict';
 
 module.exports = function(app) {
-  var dsMySQL = app.dataSources.mysqlstore;
-  dsMySQL.autoupdate(function(error) {
-    console.log('Database updated');
-  });
 };
