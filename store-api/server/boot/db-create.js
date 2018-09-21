490cfc1ea193f066d1efdf1cfe4e354c69bdf48e
Cristian Bullokles <cristian.bullokles@gmail.com>
2018-09-20 22:55:03 -0300

    Added Postgress Datasource

--- store-api/server/boot/db-create.js
+++ store-api/server/boot/db-create.js
@@ -1,4 +1,9 @@
 'use strict';
 
 module.exports = function(app) {
+  var myDB = app.dataSources.pgstoreucc;
+  myDB.autoupdate(function(result){
+    console.log('DB Created');
+    console.log(result);
+  });
 };
